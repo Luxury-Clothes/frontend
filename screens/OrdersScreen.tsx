3b911971e23c35de1b3521302b4a861278d93299
Potato <poltoradnevone@gmail.com>
2022-10-14 21:21:36 +0400

    finished orders screen

--- screens/OrdersScreen.tsx
+++ screens/OrdersScreen.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { Text, View, FlatList, TouchableOpacity } from 'react-native';
 import { useEffect } from 'react';
 
 import { useAppDispatch, useAppSelector } from '../app/hooks';
@@ -16,8 +16,58 @@ const OrdersScreen = () => {
   // console.log(orders);
 
   return (
-    <View style={{ flex: 1, backgroundColor: 'white' }}>
-      <Text>OrdersScreen</Text>
+    <View style={{ flex: 1, backgroundColor: 'white', paddingHorizontal: 20 }}>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        style={{ flexGrow: 1 }}
+        keyExtractor={(item) => item.id}
+        data={orders}
+        ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            activeOpacity={0.5}
+            className='border border-gray-100 rounded-[10px]'
+            // onPress={() => console.log('hello')}
+          >
+            <View
+              style={{
+                padding: 20,
+                backgroundColor: '#f0f9ff',
+                borderTopLeftRadius: 10,
+                borderTopRightRadius: 10,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+              }}
+            >
+              <Text style={{ fontSize: 18, fontWeight: '700' }}>
+                Заказ от 14 октября
+              </Text>
+              <Text>
+                <Text style={{ fontSize: 18, fontWeight: '700' }}>
+                  {item.total_price} Р
+                </Text>
+                , оплачено
+              </Text>
+            </View>
+            <View className='p-5 rounded-bl-[10px]  rounded-br-[10px]'>
+              <View
+                style={{ alignItems: 'center' }}
+                className='flex-row gap-2 item-center mb-2'
+              >
+                <Text className='font-bold text-[16px] '>
+                  Доставка в пункт выдачи
+                </Text>
+                <View className='text-[16px] flex-row items-center text-sm bg-blue-500 py-1 px-2 rounded-[30px] text-white'>
+                  <Text className='text-white'>Принято</Text>
+                </View>
+              </View>
+              <Text className='text-gray-600'>
+                Адресс доставки: {item.address}, {item.city}.
+              </Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 };
